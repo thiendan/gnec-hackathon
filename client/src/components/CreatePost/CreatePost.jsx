0027416d41b949504d7c073a9ec5073e05016440
139484952 <98007308+thiendan@users.noreply.github.com>
2023-07-31 10:06:27 -0400

    adding form

--- client/src/components/CreatePost/CreatePost.jsx
+++ client/src/components/CreatePost/CreatePost.jsx
@@ -7,7 +7,80 @@ const CreatePost = () => {
        <div className="container">
             <div className="content">
                 <div className="header">
-                    <span>Create a Post</span>
+                    <h1>Create a Post</h1>
+                </div>
+                <div className="body">
+                    <div className="prompt">
+                        <span>Connect with your community, one trade at a time!</span>
+                    </div>
+                    <div className="form">
+                        <div className="personal-info">
+                            <div className="name">
+                                <span>Name</span>
+                                <input 
+                                    type="text" 
+                                    placeholder="Name"
+                                />
+                            </div>
+                            
+                            <div className="location">
+                                <span>Location</span>
+                                <input 
+                                    type="text" 
+                                    placeholder="Location"
+                                />
+                            </div>
+                            
+                            <div className="number">
+                                <span>Phone Number</span>
+                                <input 
+                                    type="text" 
+                                    placeholder="Phone Number"
+                                />
+                            </div>
+                        </div>
+                        <div className="desc">
+                            <div className="trade">
+                                <span>Trade</span>
+                                <input 
+                                    type="text" 
+                                    placeholder="Trade"
+                                />
+                            </div>
+                            <div className="description">
+                                <span>Description</span>
+                                <input 
+                                    type="text" 
+                                    placeholder="Description"
+                                />
+                            </div>
+                        </div>
+                        <div className="misc">
+                            <div className="trade">
+                                <span>Expereience (in years)</span>
+                                <input 
+                                    type="text" 
+                                    placeholder="Experience"
+                                />
+                            </div>
+                            <div className="certified">
+                                <span>Certtified</span>
+                                <button>Yes</button>
+                                <button>No</button>
+                            </div>
+                        </div>
+                        <div className="images">
+                            <div className="avatar">
+                                <span>Select an avatar</span>
+                            </div>
+                            <div className="work-img">
+                                <span>Import images of your previous work</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="btn">
+                        <button>Submit</button>
+                    </div>
                 </div>
             </div>
        </div>
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -16,9 +16,12 @@ export const fetchDataFromApi = async (url) => {
     }
 };
 
-export const makePaymentRequest = axios.create({
-    baseURL: process.env.REACT_APP_DEV_URL,
-    headers: {
-        Authorization: "bearer " + process.env.REACT_APP_STRIPE_APP_KEY
-    },
-});
+// export const sendDataThruApi = async (url) => {
+//     try {
+//         const {data} = await axios.post(process.env.REACT_APP_DEV_URL + url, params);
+//         return data;
+//     } catch (error) {
+//         console.log(error);
+//         return error;
+//     }
+// };
